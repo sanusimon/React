8aaafe3e556fec8a76be22fa045e80576b792cbb
sanusimon <sanu@pgsuae.com>
2022-01-13 08:59:15 +0400

    filter

--- src/Pages/Services/ProdcutDetail.js
+++ src/Pages/Services/ProdcutDetail.js
@@ -1,11 +1,13 @@
 import {useState, useEffect} from 'react';
 import {useParams} from 'react-router-dom';
 import Header from '../../Component/Header';
+import '../Services/product.css'
 
 
 function ProdcutDetail(){
 
     const [productList , setProductList] = useState([])
+    const [loading , setLoading] = useState(true);
     const {productId} = useParams()
 
     useEffect(() => {
@@ -13,22 +15,48 @@ function ProdcutDetail(){
         .then(res=>res.json())
         .then(response => {
             setProductList(response);
+            setLoading(false);
         })
     }, [])
+
+
+    const Loading = () =>{
+        return(
+            <>
+                <h1>Loading...</h1>
+            </>
+        )
+    }
+
+    const ShowProduct = () => {
+        return(
+            <>
+                <div className="inner_">
+                        <div className="lf_">
+                            <img src={productList.image} alt='{productList.image}' />
+                        </div>
+                        <div className="rgt_">
+                           <h1>
+                               {productList.title}
+                           </h1>
+                           <p>
+                               {productList.description}
+                           </p>
+                           <label className='price'><strong>price </strong> : {productList.price}</label>
+                        </div>
+                    </div>
+            </>
+        )
+    }
+
+
     return(
         <>
             <Header />
             <section className="p_detail_">
                 <div className="container">
-                    <div className="inner_">
-                        <div className="lf_">
-                            <img src={productList.image} />
-                           
-                        </div>
-                        <div className="rgt_">
-                           
-                        </div>
-                    </div>
+                    {loading ? <Loading /> : <ShowProduct /> }
+                    
                 </div>
             </section>
         </>
--- src/Pages/Services/index.js
+++ src/Pages/Services/index.js
@@ -2,7 +2,7 @@ import Header from '../../Component/Header'
 import Banner from '../../Component/Banner'
 import { Link } from 'react-router-dom';
 import {useState , useEffect} from 'react'
-
+import '../Services/product.css'
 
 function ServicePage(){
 
@@ -13,10 +13,21 @@ function ServicePage(){
         .then(res=>res.json())
         .then(response => {
             setProductList(response);
-            
+            console.log(response)
         })
     }, [])
     
+    const uniqueTags = [];
+    const filterList = productList.map((pro,i)=>{
+        
+        if (uniqueTags.indexOf(pro.category) === -1) {
+            uniqueTags.push(pro.category)
+            return(
+                <option key={pro} value={i} >{pro.category}</option>
+            )
+        }
+        
+    })
 
     return(
         <>
@@ -25,14 +36,20 @@ function ServicePage(){
 
             <section className="service_sec">
                 <div className="container">
+                    <div className='filer_sec'>
+                        <select>
+                            <option value="">All</option>
+                            {filterList}
+                        </select>
+                    </div>
                     <div className="inner_">
-                        {productList.map((product,index)=>{
+                        {productList.map((product)=>{
                             return<div className="item" key={product.id}>
                                 <div className="img_">
                                     <img src={product.image} alt="" />
                                 </div>
                                 <div className="cap_">
-                                    <p>{product.description}</p>
+                                    <h3>{product.title}</h3>
                                     <Link to={`/services/${product.id}`}>Read More</Link>
                                 </div>
                             </div>
